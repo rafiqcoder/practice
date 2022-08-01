e3b7f21f3efef418b6302a0b9e1022e123e7ee1d
rafiqcoder <rafiqcoder@gmail.com>
2022-08-01 19:51:51 +0600

    javascript

---
+++ script.js
@@ -0,0 +1,31 @@
+//number variable
+var sunglassPrice = 120;
+//string type variable
+var specialName = "Ariana Grande";
+
+//boolean variable
+var isSingle = true;
+
+// inteager: 120
+
+//fload, decimal 12.13
+
+//variable naming
+/** 
+1. single word
+* 2. no gap
+3. no quote
+4. no reserved keyword
+5. can not start with a number
+6. cannot use dash
+7.should use camelCase
+
+shorthand: +=, -=, ++, -- 
+
+convert parseInt parseFloat
+
+toFIxed 0.22
+
+remainder : vagsesh
+**/
+
